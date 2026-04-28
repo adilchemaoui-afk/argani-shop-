808c5f9726fba9ce78669a5906c739536464af5f
adilchemaoui-afk <adilchemaoui@gmail.com>
2026-04-28 22:52:36 +0200

    Add files via upload

---
+++ app/public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'argani-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/ARGANI_bio_liege.jpg',
+  '/images/ARGANI_classique_zellige.jpg',
+  '/images/ARGANI_premium_laiton.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
